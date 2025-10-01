e426978b8b9fcfc91dd2e7faa91be80271097f4b
CrisB18 <cburitica35@gmail.com>
2025-10-01 10:46:21 -0500

    agregue un label en login

--- Portafolio_Disenadores/frmLogin.cs
+++ Portafolio_Disenadores/frmLogin.cs
@@ -16,5 +16,10 @@ namespace Portafolio_Disenadores
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
